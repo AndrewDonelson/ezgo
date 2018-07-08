867263a920a80356b8d864fb071b22d1424c635b
NlaakStudios <andrew@nlaak.com>
2018-07-07 21:40:39 -0500

    Added initial config feature

---
+++ config/config.go
@@ -0,0 +1,167 @@
+package config
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strconv"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+	"github.com/fatih/camelcase"
+	"github.com/hashicorp/hcl"
+	yaml "gopkg.in/yaml.v2"
+)
+
+//Config is the primary configuration structure for your application. Modifiy the properties as you see fit.
+type Config struct {
+	aString  string
+	aInteger int
+	aFloat   float64
+	aBoolean bool
+	aStruct  credentials
+}
+
+type credentials struct {
+	username string
+	password string
+}
+
+//AddFlags makes each configuration property available on commandline
+func (cfg *Config) AddFlags(fs *flag.FlagSet) {
+	fs.StringVar(&cfg.aString, "string", cfg.aString, "Framework user to register with the Mesos master")
+	fs.IntVar(&cfg.aInteger, "integer", cfg.aInteger, "Framework name to register with the Mesos master")
+	fs.Float64Var(&cfg.aFloat, "float", cfg.aFloat, "Framework role to register with the Mesos master")
+	fs.BoolVar(&cfg.aBoolean, "boolean", cfg.aBoolean, "Codec to encode/decode scheduler API communications [protobuf, json]")
+	fs.StringVar(&cfg.aStruct.username, "aStruct.username", cfg.aStruct.username, "Username for Mesos authentication")
+	fs.StringVar(&cfg.aStruct.password, "aStruct.passwordFile", cfg.aStruct.password, "Path to file that contains the password for Mesos authentication")
+}
+
+//DefaultConfig crates adn returns a new configuration object
+func DefaultConfig() Config {
+	return Config{
+		aString:  env("A_STRING_VALUE", "testString"),
+		aInteger: envInt("AN_INTEGER_VALUE", "5"),
+		aFloat:   envFloat("A_FLOAT_VALUE", "3.12569"),
+		aBoolean: true,
+		aStruct: credentials{
+			username: env("AUTH_USER", ""),
+			password: env("AUTH_PASSWORD_FILE", ""),
+		},
+	}
+}
+
+// LoadConfig reads configuration from path. The format is deduced from the file extension
+//	* .json    - is decoded as json
+//	* .yml     - is decoded as yaml
+//	* .toml    - is decoded as toml
+//  * .hcl	   - is decoded as hcl
+func LoadConfig(path string) (*Config, error) {
+	_, err := os.Stat(path)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := &Config{}
+	switch filepath.Ext(path) {
+	case ".json":
+		jerr := json.Unmarshal(data, cfg)
+		if jerr != nil {
+			return nil, jerr
+		}
+	case ".toml":
+		_, terr := toml.Decode(string(data), cfg)
+		if terr != nil {
+			return nil, terr
+		}
+	case ".yml":
+		yerr := yaml.Unmarshal(data, cfg)
+		if yerr != nil {
+			return nil, yerr
+		}
+	case ".hcl":
+		obj, herr := hcl.Parse(string(data))
+		if herr != nil {
+			return nil, herr
+		}
+		if herr = hcl.DecodeObject(&cfg, obj); herr != nil {
+			return nil, herr
+		}
+	default:
+		return nil, fmt.Errorf("EZGO-Config: Config file format [%s] not supported", filepath.Ext(path))
+	}
+
+	err = cfg.syncEnv()
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+// SyncEnv overrides c field's values that are set in the environment.
+//
+// The environment variable names are derived from config fields by underscoring, and uppercasing
+// the name. E.g. AppName will have a corresponding environment variable APP_NAME
+//
+// NOTE only int, string and bool fields are supported and the corresponding values are set.
+// when the field value is not supported it is ignored.
+func (cfg *Config) syncEnv() error {
+	c := reflect.ValueOf(cfg).Elem()
+	cTyp := c.Type()
+
+	for k := range make([]struct{}, cTyp.NumField()) {
+		field := cTyp.Field(k)
+
+		cm := getEnvName(field.Name)
+		env := os.Getenv(cm)
+		if env == "" {
+			continue
+		}
+		switch field.Type.Kind() {
+		case reflect.String:
+			c.FieldByName(field.Name).SetString(env)
+		case reflect.Int:
+			v, err := strconv.Atoi(env)
+			if err != nil {
+				return fmt.Errorf("EZGO-Config: Loading config field %s %v", field.Name, err)
+			}
+			c.FieldByName(field.Name).Set(reflect.ValueOf(v))
+		case reflect.Bool:
+			b, err := strconv.ParseBool(env)
+			if err != nil {
+				return fmt.Errorf("EZGO-Config: Loading config field %s %v", field.Name, err)
+			}
+			c.FieldByName(field.Name).SetBool(b)
+		}
+
+	}
+	return nil
+}
+
+// getEnvName returns all upper case and underscore separated string, from field.
+// field is a camel case string.
+//
+// example
+//	AppName will change to APP_NAME
+func getEnvName(field string) string {
+	camSplit := camelcase.Split(field)
+	var rst string
+	for k, v := range camSplit {
+		if k == 0 {
+			rst = strings.ToUpper(v)
+			continue
+		}
+		rst = rst + "_" + strings.ToUpper(v)
+	}
+	return rst
+}
---
+++ config/flags.go
@@ -0,0 +1,89 @@
+package config
+
+import (
+	"errors"
+	"net/url"
+	"strings"
+	//"github.com/mesos/mesos-go/api/v1/lib/encoding"
+	//"github.com/mesos/mesos-go/api/v1/lib/encoding/codecs"
+)
+
+var (
+	errZeroLengthLabelKey = errors.New("zero-length label key")
+)
+
+type URL struct{ url.URL }
+
+func (u *URL) Set(value string) error {
+	parsed, err := url.Parse(value)
+	if err != nil {
+		return err
+	}
+	u.URL = *parsed
+	return nil
+}
+
+// Label Key, value pair used to store free form user-data.
+type Label struct {
+	Key   string
+	Value *string
+}
+
+type Labels []Label
+
+func (labels *Labels) Set(value string) error {
+	set := func(k, v string) {
+		var val *string
+		if v != "" {
+			val = &v
+		}
+		*labels = append(*labels, Label{
+			Key:   k,
+			Value: val,
+		})
+	}
+	e := strings.IndexRune(value, '=')
+	c := strings.IndexRune(value, ':')
+	if e != -1 && e < c {
+		if e == 0 {
+			return errZeroLengthLabelKey
+		}
+		set(value[:e], value[e+1:])
+	} else if c != -1 && c < e {
+		if c == 0 {
+			return errZeroLengthLabelKey
+		}
+		set(value[:c], value[c+1:])
+	} else if e != -1 {
+		if e == 0 {
+			return errZeroLengthLabelKey
+		}
+		set(value[:e], value[e+1:])
+	} else if c != -1 {
+		if c == 0 {
+			return errZeroLengthLabelKey
+		}
+		set(value[:c], value[c+1:])
+	} else if value != "" {
+		set(value, "")
+	}
+	return nil
+}
+
+func (labels Labels) String() string {
+	// super inefficient, but it's only for occassional debugging
+	s := ""
+	valueString := func(v *string) string {
+		if v == nil {
+			return ""
+		}
+		return ":" + *v
+	}
+	for _, x := range labels {
+		if s != "" {
+			s += ","
+		}
+		s += x.Key + valueString(x.Value)
+	}
+	return s
+}
